fca62b268d02f16c72cf622c6965a8c4794abf2c
Jeffrey <jeffreysebastian@pursuit.org>
2021-04-06 22:06:38 -0400

    incomple lab

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,7 +1,42 @@
 import React from "react";
 
+const persons = [
+  {
+    name: "Jo",
+    donation: 251,
+    caption: "Do you luv me",
+  },
+  {
+    name: "John",
+    donation: 30,
+    caption: "Here take a break from work",
+  },
+  {
+    name: "Michelle",
+    donation: 30,
+    caption: "lol",
+  },
+  {
+    name: "Emily",
+    donation: 110,
+    caption: "",
+  },
+  {
+    name: "Sam",
+    donation: 30,
+    caption: "Go to Miami",
+  }
+];
 const RecentDonations = () => {
-  return null;
-};
+  // const people = persons.map(person =>{
+    return (
+      <div className="Classes">
+        <h3>Recent Donations</h3>
+        {persons.map((person) => {
+          return;
+        })}
+      </div>
+    );
+  };
 
 export default RecentDonations;
