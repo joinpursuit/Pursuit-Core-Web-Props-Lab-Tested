42cfa02b7b0fa9636378c6e4a057168a1f214e46
Miguel Catala <miguelcatala27@yahoo.com>
2021-04-06 22:04:51 -0400

    working on lab

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const DonationForm = () => {
-  return null;
+  return (
+    <div>
+    <form>
+      <input id="NameInput" placeholder="John Doe" type="text"/>
+      <input id="captionInput" placeholder="..." type="text"/>
+      <label for="donationBar">Amount</label>
+      <input type="range" id="donationBar" name="Amount" min="0" max="1000" value="5" step="10" />
+      <button type="button">Donate</button>
+
+    </form>
+    </div>
+  )
 };
 
 export default DonationForm;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const RecentDonations = () => {
-  return null;
+const RecentDonations = (props) => {
+  const { name,amount,caption } = props
+  return (
+    <div className="recentDonations">
+      <h5>Recent Donations</h5>
+    <ul>
+      <li>
+
+      <h2>{name} donated ${amount}</h2>
+      <p>{caption}</p>
+
+
+      </li>
+    </ul>
+    </div>
+  )
 };
 
 export default RecentDonations;
