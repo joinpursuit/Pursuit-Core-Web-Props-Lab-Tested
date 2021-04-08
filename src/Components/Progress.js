070957eb08d5cc33b1a7ea66097a55c23c806324
poonam <poonam398@gmail.com>
2021-04-07 23:01:13 -0400

    working on styling/css

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,11 +1,19 @@
 import React from "react";
+import"../Styles/Progress.css"
 
 const Progress = (props) => {
   const {total, goal} = props;
+  let percentage = total / goal * 100
   return (
+    <>
     <div>
       <h2>Raised ${total} of ${goal}</h2>
     </div>
+    <div className="progress-container">
+      <span className="progress-bar" width={total} height="25px"></span>
+      {/* <progress id="file" value={percentage} max="100"></progress> */}
+    </div>
+    </>
   );
 };
 
