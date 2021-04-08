5eda8aa4c8ecb788bbaa6157d878d8f632f9f7f3
joshuamendez <joshuafmendez@gmail.com>
2021-04-07 20:27:46 -0400

    code clean up

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,43 +1,5 @@
 import React from "react";
 
-// class RecentDonations extends React.Component {
-//   constructor(props){
-//     super(props);
-//   };
-
-//   render(){
-//     const { name ,amount , caption } = this.props;
-//     return (
-//       <div>
-//         <h6 role = 'heading'>
-//           Recent Donations
-//         </h6>
-//         <div>
-//           <h6 role='listitem'>
-//             {name} donated ${amount}
-//           </h6>
-//           <p>
-//             {caption}
-//           </p>
-//         </div>
-//       </div>
-//     );
-//   };
-// }
-
-// const listDonations = donations.map(donation => {
-//   total += donation.amount;
-//   return (
-//     <RecentDonations 
-//       name={donation.name}
-//       amount={donation.amount}
-//       caption={donation.caption}
-//       key={donation.id}
-//     />
-//   );
-// });
-
-
 const RecentDonations = (props) => {
   const listDonations = props.donations.map(donation => {
     return (
