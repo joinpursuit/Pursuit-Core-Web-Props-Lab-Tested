d2a4480c03d796f3a8ac63138bd7daeb02c7aad9
Teyanna Earle <teyonna.earle92@gmail.com>
2021-04-08 19:54:14 -0400

    First draft lab answers

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,7 +1,39 @@
 import React from "react";
 
-const RecentDonations = () => {
-  return null;
+
+export const DonationListItem = ({name, amount, caption}) => {
+  return (
+    <li>
+      <p>{name} donated ${amount}</p> 
+      <p>{caption}</p>
+    </li>
+  )
+}
+
+const RecentDonations = (props) => {
+  const {donations} = props
+
+  const donationS = donations.map((donation, i) => {
+    return (
+      <DonationListItem
+      key = {i}
+      name = {donation.name}
+      amount = {donation.amount}
+      caption = {donation.caption}
+      />
+    )
+  })
+
+  return (
+    <div> 
+    <h2>Recent Donations</h2>
+   <ul>
+     {donationS}
+   </ul>
+    </div>
+  );
+
+  
 };
 
 export default RecentDonations;
