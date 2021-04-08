dbb9d86823ae1bb357e70014250d53891e81642a
DamienYule <damienyule@gmail.com>
2021-04-08 02:45:43 -0400

    Damien

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,7 +1,17 @@
 import React from "react";
-
-const RecentDonations = () => {
-  return null;
+import Donation from "./Donation"
+const RecentDonations = ({donations}) => {
+  
+  const donationList = donations.map(({amount,caption,name,id}) => {
+    
+    return <Donation amount={amount} caption={caption} name={name} key={id} />
+  })
+  return (
+    <>
+    <h5 className="recentDonationsTittle">Recent Donations</h5>
+    <ul>{donationList}</ul>
+    </>
+  );
 };
 
 export default RecentDonations;
