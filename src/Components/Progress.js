ecb5cfd239777beebde1a3fbc707492bede367d9
Teyanna Earle <teyonna.earle92@gmail.com>
2021-04-08 20:34:06 -0400

    Add Bonus Answer

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const Progress = (props) => {
   const {total, goal} = props
+  let percent = ( (total / goal) * 100 ).toFixed(2)
   return (
+    <>
     <h2>Raised ${total} of ${goal}</h2>
+    <progress id="file" max={goal} value={total}>{percent}%</progress>
+    <p>{percent}%</p>
+    </>
   )
 };
 
