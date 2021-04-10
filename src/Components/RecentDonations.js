590093e35d7c03846b5edc40636bac0c21c412e1
Auredy Sanchez <auredysanchez@gmail.com>
2021-04-10 10:42:07 -0400

    in progress

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-const RecentDonations = () => {
-  return null;
+const RecentDonations = (props) => {
+  const listOfDonations = props.donations.map((list) => {
+    return (
+      <div role="listitem" key={list.id}>
+        <h6>
+          {list.name} donated ${list.amount}
+        </h6>
+        <p>{list.caption}</p>
+      </div>
+    );
+  });
+  // debugger
+  return (
+    <div>
+      <h6 role="heading">Recent Donations</h6>
+      {listOfDonations}
+      </div>
+    // <div role="heading">
+    //   <h6>{name} donated ${amount}</h6>
+    //   <p>{caption}</p>
+    // </div>
+  );
 };
 
 export default RecentDonations;
