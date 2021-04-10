b2c660d956a38e3ad544b1a2c2f5374d89a39ad1
Marsha Hall <mphall77@gmail.com>
2021-04-09 23:59:28 -0400

    add donation and form

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const DonationForm = () => {
-  return null;
+	return (
+		<form>
+			<div>
+				<label>Name</label>
+				<input type="text" placeholder="John Doe"></input>
+			</div>
+			<div>
+				<label>Caption</label>
+				<input type="text" placeholder="..."></input>
+			</div>
+			<div class="slidecontainer">
+				<label>Amount to Donate</label>
+				<input
+					type="range"
+					min="5"
+					max="1000"
+					value="5"
+					class="slider"
+					id="myRange"
+				></input>
+			</div>
+			<h3>$5</h3>
+			<button>Donate</button>
+		</form>
+	);
 };
 
 export default DonationForm;
