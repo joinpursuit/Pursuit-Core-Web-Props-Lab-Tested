d9273cee2aeaccd0d50ccca315a4cf252a57b038
Du Min Chen <dm.chen0113@gmail.com>
2021-04-11 18:13:01 -0400

    React props lab complete

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const RecentDonations = () => {
-  return null;
+const RecentDonations = props => {
+  const Lists = props.donations.map(dona => {
+    return (
+      <li className="media my-2" key={dona.id}>
+        <div className="media-body">
+          <h5 className="mt-0 mb-1">{dona.name} donated ${dona.amount}</h5>
+          {dona.caption}
+        </div>
+      </li>
+    )
+  })
+  return (
+    <ul className="col-4 list-unstyled recent-donations">
+      <h5 className='my-4'>Recent Donations</h5>
+      {Lists}
+    </ul>
+  )
 };
 
 export default RecentDonations;
