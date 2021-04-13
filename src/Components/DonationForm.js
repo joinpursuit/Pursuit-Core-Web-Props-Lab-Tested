3f25780de3921e387269d5e4c538ff38c1f077b5
Kathy Gee <mzchick@gmail.com>
2021-04-13 15:50:40 -0400

    completed lab. added some css styling

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,7 +1,28 @@
 import React from "react";
 
-const DonationForm = () => {
-  return null;
-};
-
+class DonationForm extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div >
+        <form>
+          <p>Name</p>
+          <input type="text" placeholder='John Doe'/>
+          <p>Caption</p>
+          <p>
+            <input type="text" placeholder='...'/>
+          </p>
+          <p>
+            <input type="range" id="vol" name="vol" min="5" max="1000" />
+          </p>
+          <p>
+            <button type="submit">Donate</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
 export default DonationForm;
--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const TopBar = () => {
   return (
     <section className="jumbotron text-center">
-      <h1 className="jumbotron-heading">Go Fund Me</h1>
+      <h1 className="jumbotron-heading">Go Fund <i>Me</i></h1>
       <p className="lead text-muted">
         Help me go on a vacation to a beach somewhere
       </p>
