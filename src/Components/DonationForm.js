059bbc9b2126c803c4908264aa9c4c619231d791
Hady Mohamed <hadymohamed@pursuit.org>
2021-04-16 15:29:32 -0400

    formatted js files

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -3,18 +3,18 @@ import React from "react";
 const DonationForm = () => {
   return (
     <div>
-     <div>
-      <form>
-        Name
-        <input type="text" placeholder="John Doe"/>
-        Caption
-        <input type="text" placeholder="..."/>
-        <input type="range" name="range" min="5" max="1000" />
-        <button>Donate</button>
-      </form>
+      <div>
+        <form>
+          Name
+          <input type="text" placeholder="John Doe" />
+          Caption
+          <input type="text" placeholder="..." />
+          <input type="range" name="range" min="5" max="1000" />
+          <button>Donate</button>
+        </form>
+      </div>
     </div>
-    </div> 
-  )
+  );
 };
 
 export default DonationForm;
