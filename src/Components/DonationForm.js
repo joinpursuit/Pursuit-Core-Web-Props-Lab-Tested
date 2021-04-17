1502231275d8a8843215fb86162f5a74fb28a2b9
cassidybeni <cassidybeni@pursuit.org>
2021-04-17 09:51:51 -0400

    Add Donate button

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -7,6 +7,7 @@ const DonationForm = () => {
       <input type="text" placeholder="John Doe"></input>
       <input type="text" placeholder="..."></input>
       <input type="range" min="5" max="1000"></input>
+      <button>Donate</button>
     </div>
   )
 };
