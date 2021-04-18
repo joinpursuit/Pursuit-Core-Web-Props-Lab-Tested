44f272eac547587bbef8c991c97434d48f92f984
Jarrett Harris <Jarrettharris@pursuit.org>
2021-04-18 13:55:53 -0400

    lab completed

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 const DonationForm = () => {
-  return null;
+  return (
+    <form>
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="nameInput"
+          placeholder="John Doe"
+        />
+      </div>
+      <div className="form-group">
+        <label>Caption</label>
+        <input
+          type="text"
+          className="form-control"
+          id="captionInput"
+          placeholder="..."
+        />
+      </div>
+      <div className="form-group">
+        <label>Amount to Donate</label>
+        <input
+          type="range"
+          className="custom-range"
+          id="amountInput"
+          min="5"
+          max="1000"
+        />
+        
+          <p>$5</p>
+          <button type="button" className="btn btn-lg btn-success my-4">
+            Donate
+          </button>
+        
+      </div>
+    </form>
+  )
 };
 
 export default DonationForm;
