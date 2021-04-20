def7c1986501bf80bb94432b5baa8e77c6e581d2
Kelvin Zheng <kzheng000@citymail.cuny.edu>
2021-04-20 18:26:51 -0400

    test completed

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const RecentDonations = () => {
-  return null;
-};
+const RecentDonations = (props) => {
+  const {donations} = props
+  const groupDonations = donations.map((dono, i) => {
+    return (
+      <li key={i}>
+        <h3>{dono.name} donated ${dono.amount}</h3>
+        <p>{dono.caption}</p>
+      </li>
+    )})
+  return (
+    <div>
+      <h1>Recent Donations</h1>
+      {groupDonations}
+    </div>
+  )
+}
 
 export default RecentDonations;
