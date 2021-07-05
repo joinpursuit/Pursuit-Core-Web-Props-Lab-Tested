6946ceff14f3e4055c40e6b06951898db61042d3
Grace Bong <gracebong.park@gmail.com>
2021-07-05 16:54:07 -0400

    minor change

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -6,8 +6,12 @@ const DonationForm = () => {
       <div>
         <form>
           Name
+          <br/>
           <input type="text" placeholder="John Doe"/>
+          <br/>
+          <br/>
           Caption
+          <br/>
           <input type="text" placeholder="..."/>
           <input type="range" name="range" min="5" max="1000"></input>
           <button>Donate</button>
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,3 @@
-
-
 const RecentDonations = (props) => {
   const { donations } = props
 
